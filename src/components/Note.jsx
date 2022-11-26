b3e42c3977e0bd1769236e57f5d7ac1cf121ad84
mahimagupta24 <mahimagupta2401@gmail.com>
2022-11-26 13:05:11 +0530

    refactor

---
+++ src/components/Note.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Note(){
+    return (
+        <note>
+        <div>
+            <h1>Title</h1>
+            <p>content</p>
+
+        </div>
+        </note>
+    )
+}
+export default Note;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import App from './components/App';
 
 
 
-ReactDOM.render(<h1>Hello</h1>,document.getElementById('root'));
+
+ReactDOM.render(<App />,document.getElementById('root'));
